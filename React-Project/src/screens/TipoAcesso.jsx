d592b46d1292ef46538394e32588e9a837744349
José Roberto <joseroberto.devops@gmail.com>
2025-04-18 00:19:52 -0300

    first commit

---
+++ React-Project/src/screens/TipoAcesso.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './LoginStyle/TipoAcesso.css'
+
+function TelaTipoAcesso() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>AQUI VAI O HTML</h1>
+      </div>
+    </>
+  )
+}
+
+export default TelaTipoAcesso
